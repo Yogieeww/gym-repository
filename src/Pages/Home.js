7c28d70183615e93b954be20dfe7a1ba9edf6921
Yogieeww <127382080+Yogieeww@users.noreply.github.com>
2024-05-12 19:39:57 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -49,7 +49,7 @@ const Home = () => {
             <Col md={5} className="g-3" style={{ paddingBottom: 20 }}>
               <img
                 src={homeLogo}
-                alt="home pic"
+                alt="amico"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
               />
